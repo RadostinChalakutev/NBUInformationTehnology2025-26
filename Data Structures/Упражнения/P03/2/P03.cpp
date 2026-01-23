b3436eea8ee6195e57aac4e4084aa35f2d40c945
Radostin Chalakutev <radostinchalakutev@gmail.com>
2026-01-23 21:48:02 +0200

    100

--- Data Structures/Упражнения/P03/2/P03.cpp
+++ Data Structures/Упражнения/P03/2/P03.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 using namespace std;
 
+template <typename T>
+class Deque {
+private:
+    struct Node {
+        T data;
+        Node* prev;
+        Node* next;
+
+        Node(const T& data) : data(data), prev(NULL), next(NULL) {};
+    };
+    Node* head;
+    Node* tail;
+    size_t sz;
+    public:
+    Deque() : head(NULL), tail(NULL), sz(0) {};
+
+    ~Deque() {clear();}
+    Deque(const)
+};
