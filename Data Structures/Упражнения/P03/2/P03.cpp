a0c056a8073a99e183bdf256a3d7518d2b13187f
Radostin Chalakutev <radostinchalakutev@gmail.com>
2026-01-30 01:24:51 +0200

    100

--- Data Structures/Упражнения/P03/2/P03.cpp
+++ Data Structures/Упражнения/P03/2/P03.cpp
@@ -14,9 +14,55 @@ private:
     Node* head;
     Node* tail;
     size_t sz;
-    public:
+public:
     Deque() : head(NULL), tail(NULL), sz(0) {};
 
     ~Deque() {clear();}
-    Deque(const)
+
+    bool empty() const {
+        return sz==0;
+
+    }
+    size_t size() const {return sz;}
+
+    void insertFront(const T& e) {
+        Node* n =new Node(e);
+
+        if (empty()) {
+            head=tail=n;
+        }else {
+            n->next=head;
+            head->prev=n;
+            head=n;
+        }
+        sz++;
+    }
+    void insertBack(const T& e) {
+        Node* n =new Node(e);
+        if (empty()) {
+            head=tail=n;
+
+        }else {
+            n->prev=tail;
+            tail->next=n;
+            tail=n;
+        }
+        sz++;
+    }
+
+    void eraseFront() {
+        if (empty()) {
+            throw std::out_of_range("Deque is empty");
+
+        }
+        Node* old=head;
+        if (head==tail) {
+            head=tail=NULL;
+        }else {
+            head=head->next;
+            head->prev=NULL;
+        }
+        delete old;
+        sz--
+    }
 };
